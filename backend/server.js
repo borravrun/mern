a1a1b0ba1264f366f1d4626d3fa7617a72c81bf7
Varun <varunchowdharyborra@gmail.com>
2025-09-04 22:21:58 +0530

    🗄️Setup frontend with Vite, React, and Chakra UI; add initial components and styles

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import 'dotenv/config';
 import app from './app.js';
 import { connectDB } from './config/db.js';
 
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(port, () => {
   connectDB();
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${port}`);
 });
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import {Button} from "@chakra-ui/react";
+
+export default function App(){
+    return(
+        <div>
+            <Button>
+                click me
+            </Button>
+        </div>
+    )
+}
