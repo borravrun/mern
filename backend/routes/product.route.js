97a693f688281082dc065c9d2247754e0afe6fa3
Varun <varunchowdharyborra@gmail.com>
2025-09-04 09:42:31 +0530

    🗄️Backend setup: add product controller and routes

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,6 +1,4 @@
 import {Router} from "express";
-import Product from "../model/product.model.js";
-import mongoose from "mongoose";
 import {createProduct, deleteProductById, getProducts, updateProductById} from "../controller/product.controller.js";
 
 const router = Router();
