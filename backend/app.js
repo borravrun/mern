fd672ee293be7ea4b3f671f78e48a45a115c8bb9
Varun <varunchowdharyborra@gmail.com>
2025-09-05 15:15:32 +0530

    Update .gitignore and enhance backend app configuration; add build and start scripts to root package.json

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 import express from 'express';
 import productRoute from "./routes/product.route.js";
+import path from 'path';
 
 const app = express();
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV === 'production'){
+    console.log(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+    app.get('/{*any}',(req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    })
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
